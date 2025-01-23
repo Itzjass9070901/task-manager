bb4279a4ccbf6e0422b78374dc549b5374a822f8
Itzjass9070901 <jassielito2007@gmail.com>
2025-01-23 13:09:11 -0700

    Update task.js

--- task.js
+++ task.js
@@ -1,54 +1,61 @@
 // Global Variable and will create and empty array with name tasks
 
-let tasks=[];
+let tasks = [];
 
 // AddTask Function
-function addTask(){
-const input = document.getElementById('taskInput');
-const task = input.value.trim();
-console.log(task);
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const task = input.value.trim();
+  console.log(task);
 
-if(task !=='')
-{
+  if (task !== "") {
     tasks.push(task);
     console.log(tasks);
-    input.value='';
+    input.value = "";
     displayTasks();
-}
-}
- function removeTask(index){
-    tasks.splice(index,1);
-    displayTasks();
-
+  }
 }
 
+//Edit Task
+function editTask(index) {
+  const newTask = prompt("Edit Task", tasks[index]);
+  console.log(newTask);
+  tasks[index] = newTask.trim();
+  displayTasks()
+}
 
+function removeTask(index) {
+  tasks.splice(index, 1);
+  displayTasks();
+  tasks[index]=newsTask.trim();
+  displayTasks();
+}
 
 // Display Tasks-
-function displayTasks(){
-    const taskList= document.getElementById('taskList');
-    let html='';
+function displayTasks() {
+  const taskList = document.getElementById("taskList");
+  let html = "";
 
-    for(let i=0; i<tasks.length;i++){
-        html += `
+  for (let i = 0; i < tasks.length; i++) {
+    html += `
             <div class="task-item">
-                <span class="task-text"> ${i+1}. ${tasks[i]}</span>    
+                <span class="task-text"> ${i + 1}. ${tasks[i]}</span>    
            
             <div class="button-group">
             
+            <button class="edit-btn" onclick="editTask(${i})" >Edit </button>
             
             <button class="delete-btn" onclick="removeTask(${i})" >Delete </button>
+            
+           
+           
             </div>
         
             </div>
         
         
         `;
-    
-    }
+  }
 
-
-
-    taskList.innerHTML = html;
-
+  taskList.innerHTML = html;
 }
